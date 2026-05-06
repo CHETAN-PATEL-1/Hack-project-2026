16f6182424636a7737488e7e258ece63fbed571b
Chetan <180172390+CHETAN-PATEL-1@users.noreply.github.com>
2026-05-06 22:41:41 +0530

    fix button bg-color

--- Home Page/Script.js
+++ Home Page/Script.js
@@ -1,23 +1,23 @@
 let x = document.querySelector("#temp1");
-x.style.backgroundColor = "rgb(15, 131, 15)";
+x.style.backgroundColor = "rgb(45, 142, 0)";
 x.style.color = "white";
 x.addEventListener("mouseenter",function(){
-    x.style.backgroundColor = "rgb(89, 194, 29)";
+    x.style.backgroundColor = "rgb(72, 163, 20)";
     x.style.color = "black";
 })
 x.addEventListener("mouseleave",function(){
-    x.style.backgroundColor = "rgb(15, 131, 15)";
+    x.style.backgroundColor = "rgb(42, 142, 0)";
     x.style.color = "white";
 })
 
 let y = document.querySelector("#temp2");
-y.style.backgroundColor = "rgb(15, 131, 15)";
+y.style.backgroundColor = "rgb(15, 142, 0)";
 y.style.color = "white";
 y.addEventListener("mouseenter",function(){
-    y.style.backgroundColor = "rgb(89, 194, 29)";
+    y.style.backgroundColor = "rgb(72, 163, 20)";
     y.style.color = "black";
 })
 y.addEventListener("mouseleave",function(){
-    y.style.backgroundColor = "rgb(15, 131, 15)";
+    y.style.backgroundColor = "rgb(15, 142, 0)";
     y.style.color = "white";
 })
